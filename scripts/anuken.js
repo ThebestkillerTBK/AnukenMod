476242b45248d95fcab614988bf19cc3d377781d
2593828650 <2593828650@qq.com>
2021-05-23 16:55:26 +0800

    v1.1

--- scripts/anuken.js
+++ scripts/anuken.js
@@ -1,4 +1,112 @@
-const anuken1 = require("coder-anuken");
+const codegun = extend(Weapon, {
+  name: "anukenisgod-code",
+  shootSound: Sounds.plasmadrop,
+  x: -3,
+  y: 7,
+  reload: 15,
+  recoil: 2,
+  rotate: true,
+  mirror: true,
+  rotateSpeed: 20,
+  ejectEffect: Fx.casing4
+});
+
+const code = extend(BasicBulletType, {
+  speed: 4.7,
+  splashDamageRadius: 80,
+  splashDamage: 40,
+  width: 25,
+  height: 25,
+  homingPower: 0.2,
+  homingRange: 150,
+  lifetime: 140,
+  damage: 256,
+  targetAir: true,
+  shake: 1,
+  hitEffect: Fx.blastExplosion,
+  collidesTeam:true,
+  healPercent: 2,
+  knockback: 0.8,
+  statusDuration: 60,
+  status: StatusEffects.blasted,
+  backColor: Color.valueOf("#005500"),
+  frontColor: Color.valueOf("#007700"),
+});
+
+const lasereye = extend(Weapon, {
+  inaccuracy: 360,
+  shootSound: Sounds.laserblast,
+  reload: 300,
+  shots: 18,
+});
+
+const lasereyebullet = extend(LaserBulletType, {
+damage: 500,
+length: 400,
+width: 60,
+height: 8,
+lifetime: 120,
+lightningSpacing: 50,
+lightningLength: 15,
+lightningDelay: 0.7,
+lightningLengthRand: 10,
+lightningDamage: 128,
+lightningAngleRand: 30,
+pierce: true,
+color: Color.valueOf("207740"),
+reflectable: false,
+statusDuration: 120,
+status: StatusEffects.disarmed
+});
+
+const eyesx = extend(Weapon, {
+  firstShotDelay: 10,
+  shootSound: Sounds.laserbig,
+  reload: 250,
+  continuous: true
+});
+
+const eyegunx = extend(ContinuousLaserBulletType, {
+  length: 300,
+  width: 13,
+  lifetime: 160,
+  damage: 30,
+  colors: [Color.valueOf("#005500"), Color.valueOf("#007700")],
+  shootEffect: Fx.shootBigSmoke2,
+  shake: 3,
+  hitEffect: Fx.hitMeltHeal,
+  statusDuration: 80,
+  status: StatusEffects.sapped,
+  collidesTeam:true,
+  healPercent: 1
+});
+        
+lasereye.bullet = lasereyebullet;
+codegun.bullet = code;
+eyesx.bullet = eyegunx;
+
+const coderAnuken = extendContent(UnitType, "coder-anuken", {
+  alwaysUnlocked: true,
+  speed: 3.5,
+  flying: true,
+  health: 32768,
+  hitSize: 10,
+  description: "[crimson]The true Anuken, the programmer Anuken. \n[forest]However, he is lazy.",
+  buildSpeed: 9999,
+  rotateSpeed: 10,
+  itemCapacity: 256,
+  commandLimit: 10,
+});
+
+coderAnuken.constructor = () => extend(UnitEntity, {});
+
+coderAnuken.weapons.add(codegun, lasereye, eyesx);
+
+coderAnuken.defaultController = () => new FlyingAI();
+
+const lightning1 = new MoveLightningAbility(32, 40, 0.8, 0, 2, 4, Color.valueOf("#90aa90"))
+coderAnuken.abilities.add(lightning1);
+
 const eyes = extend(Weapon, {
   firstShotDelay: 10,
   shootSound: Sounds.laserbig,
@@ -72,7 +180,7 @@ anuken.constructor = () => extend(MechUnit, {
   killed(){
     this.super$killed();
     print("THE CODER ANUKEN USE THE CODE TO ATTACK YOU");
-    anuken1.coderAnuken.spawn(this.team, this.x, this.y);
+    coderAnuken.spawn(this.team, this.x, this.y);
   }});
 
 anuken.weapons.add(eyes,eyes2);
